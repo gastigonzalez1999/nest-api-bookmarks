c5dc276c712aab40eae4a2fc17c62507cce240d6
Gasti <gaston.gonzalez@paydock.com>
2024-05-25 19:54:56 -0300

    Update modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, UserModule, BookmarkModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
